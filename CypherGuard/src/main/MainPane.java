66d4e48d5c1a04be235248cebc2a8185630366ed
Tim Rascol <timothyrascol18@gmail.com>
2024-03-13 17:09:57 -0400

    Made some graphical changes
    Added some UI and some implementation for the buttons. I have an idea of what I want to do, just learning how to use javaFX GUI. The logo does not look good and we'll have to find a way to make a good looking sharp one.

--- CypherGuard/src/main/MainPane.java
+++ CypherGuard/src/main/MainPane.java
@@ -1,41 +1,57 @@
 package main;
 
-
+import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
 
 public class MainPane extends GridPane {
 
-    // Create GUI elements of the main menu
     public MainPane() {
-        
         // Center vertically
         setAlignment(Pos.CENTER);
 
-        // Title Label
-        Label titleLabel = new Label("Cypher Guard"); // Label Text
-        titleLabel.setFont(Font.font("Arial", 48)); // Font and Size
-        titleLabel.setTextFill(javafx.scene.paint.Color.WHITE); // Font Color
-        setHalignment(titleLabel, HPos.CENTER); // Center the text
-        add(titleLabel, 0, 0, 2, 1); // Span it two columns
+        // Logo Code
+        File file = new File("img/CypherGuardLogo.png");
+        Image logoImg = new Image(file.toURI().toString());
+        ImageView logoImageView = new ImageView(logoImg);
+        add(logoImageView, 0, 0);
 
         // Buttons
-        Button savePasswordButton = new Button("Save a Password");
-        Button viewPasswordsButton = new Button("Look at Passwords");
-        Button saveNoteButton = new Button("Save a Note");
+        Button btnSavePassword = new Button("Save a Password");
+        Button btnViewPassword = new Button("Look at Passwords");
+        Button btnSaveNote = new Button("Save a Note");
+        Button btnClose = new Button("Close");
 
-        // Arrage the buttons in the center
-        GridPane buttonPane = new GridPane();
-        buttonPane.add(savePasswordButton, 0, 0);
-        buttonPane.add(viewPasswordsButton, 1, 0);
-        buttonPane.add(saveNoteButton, 2, 0);
-        setHalignment(buttonPane, HPos.RIGHT);
-        add(buttonPane, 0, 1, 2, 1); // Add buttonPane to the main grid, spanning two columns
-        
-        setStyle("-fx-background-color: #091232"); // Sets background to be dark blue
+        // CSS Styling
+        btnSavePassword.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        btnViewPassword.setStyle("-fx-background-color: #008CBA; -fx-text-fill: white;");
+        btnSaveNote.setStyle("-fx-background-color: #f44336; -fx-text-fill: white;");
+        btnClose.setStyle("-fx-background-color: #555555; -fx-text-fill: white;");
+
+        // Button actions
+        btnClose.setOnAction(e -> Platform.exit());
+
+        // Button Pane Design
+        VBox buttonBox = new VBox(10); // Spacing between buttons
+        buttonBox.setAlignment(Pos.TOP_LEFT);
+        buttonBox.getChildren().addAll(btnSavePassword, btnViewPassword, btnSaveNote, btnClose);
+        add(buttonBox, 0, 2);
+
+        // Set column constraints to make the button pane wider
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        columnConstraints.setHgrow(Priority.ALWAYS);
+        getColumnConstraints().add(columnConstraints);
+
+        // Background styling
+        setStyle("-fx-background-color: #091232");
     }
 }
